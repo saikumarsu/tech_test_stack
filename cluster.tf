cb640b3faceff3ba1e995cd88c1e4b12d1f8bfc2
EC2 Default User <ec2-user@ip-172-31-37-149.eu-west-1.compute.internal>
2022-05-04 18:08:38 +0000

    ecs cluster configuration added

---
+++ cluster.tf
@@ -0,0 +1,24 @@
+# Providing a reference to our default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "eu-west-2a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "eu-west-2b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "eu-west-2c"
+}
+
+resource "aws_ecr_repository" "webservice_ecr_repo" {
+  name = "webservice-ecr-repo"
+}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "ecs-cluster-01" # Naming the cluster
+}
